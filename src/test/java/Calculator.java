11f8d5b7fe814a3dd89c66f5625d40892b811b2f
AutomationTester19 <Justypedigvijay@gmail.com>
2026-09-18 13:37:10 +0530

    basic java docker project

--- src/test/java/Calculator.java
+++ src/test/java/Calculator.java
@@ -3,6 +3,7 @@ public class Calculator {
 
     public static void main(String[] args) {
 
+        System.out.println("Welcome to Calculator APP: ");
        int a = 10;
        int b = 20;
        int c = add(a,b);
