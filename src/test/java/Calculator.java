1c78b5fa48ad4a102986ebe6a210b0d7e07f42e4
AutomationTester19 <Justypedigvijay@gmail.com>
2026-09-18 13:23:20 +0530

    basic java docker project

---
+++ src/test/java/Calculator.java
@@ -0,0 +1,37 @@
+public class Calculator {
+
+
+    public static void main(String[] args) {
+
+       int a = 10;
+       int b = 20;
+       int c = add(a,b);
+       System.out.println("Addition of " + a + " and " + b + " is: " + c);
+
+       int d = sub(a,b);
+        System.out.println("Subtraction of " + a + " and " + b + " is: " + d);
+
+        int e = mul(a,b);
+        System.out.println("Multiplication of " + a + " and " + b + " is: " + e);
+
+        int f = div(a,b);
+        System.out.println("Division of " + b + " and " + a + " is: " + f);
+    }
+
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    public static int sub(int a, int b){
+        return b-a;
+    }
+
+    public static int mul(int a, int b){
+        return a*b;
+    }
+
+    public static int div(int a, int b){
+        return b/a;
+    }
+}
